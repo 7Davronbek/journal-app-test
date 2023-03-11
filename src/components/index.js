c254483b0ad2b2fd7cc037b2bedf0fae57eca8a8
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-03-11 23:58:22 +0300

    journal-app

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as LoginForm } from "./login/LoginForm";
 export { default as AdminLayout } from "./admin/AdminLayout";
 export { default as JournalMain } from "./journals/JournalMain";
+export { default as JournalCards } from "./journals/JournalCards";
---
+++ src/components/journals/JournalCards.jsx
@@ -0,0 +1,250 @@
+import React from "react";
+
+const JournalCards = () => {
+  return (
+    <>
+      <div className="col-lg-6">
+        <div className="cards">
+          <div className="img">
+            <img src="/images/card.png" alt="" />
+          </div>
+          <div className="mid">
+            <h2>001</h2>
+            <h3>
+              Ўзбекистон Фанлар Академияси, У.А. Арифов номидаги Ион-плазма ва
+              лазер технологиялари институти
+            </h3>
+            <h4>
+              Ключевое слово: <b>Физика / Точные науки / Узбекистан</b>
+            </h4>
+
+            <div className="cardFooter">
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/calendar.svg" alt="" />
+                </i>
+                <p>12.12.2022</p>
+              </div>
+
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/download.svg" alt="" />
+                </i>
+                <p>200 000</p>
+              </div>
+
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/eye.svg" alt="" />
+                </i>
+                <p>200 000</p>
+              </div>
+            </div>
+          </div>
+          <div className="editWrap">
+            <button className="btn edit">
+              <img src="/icons/edit.svg" alt="" />
+            </button>
+            <button className="btn delete">
+              <img src="/icons/delete.svg" alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6">
+        <div className="cards">
+          <div className="img">
+            <img src="/images/card.png" alt="" />
+          </div>
+          <div className="mid">
+            <h2>001</h2>
+            <h3>
+              Ўзбекистон Фанлар Академияси, У.А. Арифов номидаги Ион-плазма ва
+              лазер технологиялари институти
+            </h3>
+            <h4>
+              Ключевое слово: <b>Физика / Точные науки / Узбекистан</b>
+            </h4>
+
+            <div className="cardFooter">
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/calendar.svg" alt="" />
+                </i>
+                <p>12.12.2022</p>
+              </div>
+
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/download.svg" alt="" />
+                </i>
+                <p>200 000</p>
+              </div>
+
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/eye.svg" alt="" />
+                </i>
+                <p>200 000</p>
+              </div>
+            </div>
+          </div>
+          <div className="editWrap">
+            <button className="btn edit">
+              <img src="/icons/edit.svg" alt="" />
+            </button>
+            <button className="btn delete">
+              <img src="/icons/delete.svg" alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6">
+        <div className="cards">
+          <div className="img">
+            <img src="/images/card.png" alt="" />
+          </div>
+          <div className="mid">
+            <h2>001</h2>
+            <h3>
+              Ўзбекистон Фанлар Академияси, У.А. Арифов номидаги Ион-плазма ва
+              лазер технологиялари институти
+            </h3>
+            <h4>
+              Ключевое слово: <b>Физика / Точные науки / Узбекистан</b>
+            </h4>
+
+            <div className="cardFooter">
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/calendar.svg" alt="" />
+                </i>
+                <p>12.12.2022</p>
+              </div>
+
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/download.svg" alt="" />
+                </i>
+                <p>200 000</p>
+              </div>
+
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/eye.svg" alt="" />
+                </i>
+                <p>200 000</p>
+              </div>
+            </div>
+          </div>
+          <div className="editWrap">
+            <button className="btn edit">
+              <img src="/icons/edit.svg" alt="" />
+            </button>
+            <button className="btn delete">
+              <img src="/icons/delete.svg" alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6">
+        <div className="cards">
+          <div className="img">
+            <img src="/images/card.png" alt="" />
+          </div>
+          <div className="mid">
+            <h2>001</h2>
+            <h3>
+              Ўзбекистон Фанлар Академияси, У.А. Арифов номидаги Ион-плазма ва
+              лазер технологиялари институти
+            </h3>
+            <h4>
+              Ключевое слово: <b>Физика / Точные науки / Узбекистан</b>
+            </h4>
+
+            <div className="cardFooter">
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/calendar.svg" alt="" />
+                </i>
+                <p>12.12.2022</p>
+              </div>
+
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/download.svg" alt="" />
+                </i>
+                <p>200 000</p>
+              </div>
+
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/eye.svg" alt="" />
+                </i>
+                <p>200 000</p>
+              </div>
+            </div>
+          </div>
+          <div className="editWrap">
+            <button className="btn edit">
+              <img src="/icons/edit.svg" alt="" />
+            </button>
+            <button className="btn delete">
+              <img src="/icons/delete.svg" alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="col-lg-6">
+        <div className="cards">
+          <div className="img">
+            <img src="/images/card.png" alt="" />
+          </div>
+          <div className="mid">
+            <h2>001</h2>
+            <h3>
+              Ўзбекистон Фанлар Академияси, У.А. Арифов номидаги Ион-плазма ва
+              лазер технологиялари институти
+            </h3>
+            <h4>
+              Ключевое слово: <b>Физика / Точные науки / Узбекистан</b>
+            </h4>
+
+            <div className="cardFooter">
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/calendar.svg" alt="" />
+                </i>
+                <p>12.12.2022</p>
+              </div>
+
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/download.svg" alt="" />
+                </i>
+                <p>200 000</p>
+              </div>
+
+              <div className="d-flex align-items-center">
+                <i className="me-2">
+                  <img src="/icons/eye.svg" alt="" />
+                </i>
+                <p>200 000</p>
+              </div>
+            </div>
+          </div>
+          <div className="editWrap">
+            <button className="btn edit">
+              <img src="/icons/edit.svg" alt="" />
+            </button>
+            <button className="btn delete">
+              <img src="/icons/delete.svg" alt="" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default JournalCards;
