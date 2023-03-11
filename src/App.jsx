7463d3eb1d6cc2a2157af3de4cd1f9cbbab5cba6
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-03-12 00:09:45 +0300

    journal-app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { CreateJournal, Journals, Login } from "./pages";
+import { ScrollToTop } from "./components";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       </Routes>
 
       <ToastContainer />
+      <ScrollToTop />
     </>
   );
 };
