30133ef7113668840ee9ccb585ac62452fe4d145
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-03-12 11:58:32 +0300

    journal-app

--- src/components/journals/CreateJournalRu.jsx
+++ src/components/journals/CreateJournalRu.jsx
@@ -1,30 +1,65 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 
 const CreateJournalRu = ({ back }) => {
+  const [pdf_file, setPdf_file] = useState("");
+  const [image, setImage] = useState("");
+
+  const name_ru = useRef("");
+  const description_ru = useRef("");
+  const keyword_ru = useRef("");
+  const date = useRef("");
+
+  const createJournal = (e) => {
+    e.preventDefault();
+    const data = {
+      name_ru: name_ru.current.value,
+      description_ru: description_ru.current.value,
+      keyword_ru: keyword_ru.current.value,
+      date: date.current.value,
+      image,
+      pdf_file,
+      date,
+    };
+    console.log(data);
+  };
+
   return (
     <div className="CreateJournalRu">
-      <div className="row">
+      <form onSubmit={createJournal} className="row">
         <div className="col-12">
           <h1>Добавить журналы</h1>
         </div>
         <div className="col-lg-3">
           <div className="imgWrap">
             <input
+              required
               accept="image/png, image/jpeg, image/jpg, image/webp, image/svg"
               id="img"
               type="file"
               className="img"
+              onChange={(e) => setImage(e.target.files[0])}
             />
+
             <label htmlFor="img">
-              <img src="/icons/photo.svg" alt="" />
+              {image ? (
+                <img
+                  src={URL.createObjectURL(image)}
+                  style={{ width: "100%", height: "337px", objectFit: "cover" }}
+                  alt=""
+                />
+              ) : (
+                <img src="/icons/photo.svg" alt="" />
+              )}
             </label>
           </div>
           <div className="fileWrap">
             <input
-              accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+              required
+              accept=".pdf"
               type="file"
               className="file"
               id="file"
+              onChange={(e) => setPdf_file(e.target.files[0])}
             />
             <label htmlFor="file">
               Добавить файл{" "}
@@ -32,12 +67,20 @@ const CreateJournalRu = ({ back }) => {
                 <img src="/icons/download.svg" alt="" />
               </span>
             </label>
+            {pdf_file && pdf_file.name}
           </div>
         </div>
+
         <div className="col-lg-9">
           <div className="d-flex align-items-center">
-            <input required type="date" className="form-control me-3" />
             <input
+              ref={date}
+              required
+              type="date"
+              className="form-control me-3"
+            />
+            <input
+              ref={keyword_ru}
               required
               type="text"
               placeholder="Ключевое слово"
@@ -45,12 +88,14 @@ const CreateJournalRu = ({ back }) => {
             />
           </div>
           <input
+            ref={name_ru}
             required
             type="text"
             placeholder="Название журнала"
             className="form-control"
           />
           <textarea
+            ref={description_ru}
             required
             className="form-control"
             placeholder="Описание"
@@ -68,7 +113,7 @@ const CreateJournalRu = ({ back }) => {
             </button>
           </div>
         </div>
-      </div>
+      </form>
     </div>
   );
 };
