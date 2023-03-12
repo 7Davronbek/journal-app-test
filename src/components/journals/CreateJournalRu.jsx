dc02e010802692e579e266f19a743b2265cb14e1
7Davronbek <maxmudovdavronbek8@gmail.com>
2023-03-12 11:14:24 +0300

    journal-app

--- src/components/journals/CreateJournalRu.jsx
+++ src/components/journals/CreateJournalRu.jsx
@@ -7,34 +7,53 @@ const CreateJournalRu = ({ back }) => {
         <div className="col-12">
           <h1>Добавить журналы</h1>
         </div>
-        <div className="col-lg-2">
+        <div className="col-lg-3">
           <div className="imgWrap">
-            <input id="img" type="file" className="img"></input>
+            <input
+              accept="image/png, image/jpeg, image/jpg, image/webp, image/svg"
+              id="img"
+              type="file"
+              className="img"
+            />
             <label htmlFor="img">
               <img src="/icons/photo.svg" alt="" />
             </label>
           </div>
           <div className="fileWrap">
-            <input type="file" className="file" id="file" />
-            <label htmlFor="file"></label>
+            <input
+              accept=".doc,.docx,application/msword,application/vnd.openxmlformats-officedocument.wordprocessingml.document"
+              type="file"
+              className="file"
+              id="file"
+            />
+            <label htmlFor="file">
+              Добавить файл{" "}
+              <span className="ms-2">
+                <img src="/icons/download.svg" alt="" />
+              </span>
+            </label>
           </div>
         </div>
-        <div className="col-lg-10">
+        <div className="col-lg-9">
           <div className="d-flex align-items-center">
-            <input type="date" className="form-control" />
+            <input required type="date" className="form-control me-3" />
             <input
+              required
               type="text"
               placeholder="Ключевое слово"
               className="form-control"
             />
           </div>
           <input
+            required
             type="text"
             placeholder="Название журнала"
             className="form-control"
           />
           <textarea
+            required
             className="form-control"
+            placeholder="Описание"
             id=""
             cols="30"
             rows="10"
@@ -44,7 +63,9 @@ const CreateJournalRu = ({ back }) => {
             <button onClick={back} className="btn back">
               Отменить
             </button>
-            <button className="btn save">Сохранить</button>
+            <button type="submit" className="btn save">
+              Сохранить
+            </button>
           </div>
         </div>
       </div>
